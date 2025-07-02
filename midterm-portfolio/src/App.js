3611a22b3d2a5402d5a16c07c06a22162d4286cf
Rodrigo Moreno <morenorodrigo541@gmail.com>
2025-07-01 23:14:23 -0700

    Structure of Website

---
+++ midterm-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Aboutme from './components/Aboutme/Aboutme';
+import Main from './components/Main/Main';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Main></Main>
+      <Aboutme></Aboutme>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ midterm-portfolio/src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+
+    let name = "Rodrigo";
+    let web = "rodrigo.com"
+
+  return (
+    <div>
+           <div>
+        <h1>Home</h1>
+        <p>My name is {name}</p>
+        <p>My web is {web}</p>
+    </div>
+    </div>
+  )
+}
+export default Main;
